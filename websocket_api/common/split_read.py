efc66f5f63ce245c06a440b4d84adc63212d7667
leen0910 <16955414@qq.com>
2019-05-22 17:37:01 +0800

    common/split_read.py: 按size大小分包读取文件，并计算总包数

---
+++ websocket_api/common/split_read.py
@@ -0,0 +1,37 @@
+
+# encoding: utf-8
+import os
+
+def total_count(fpath,Block_Size):
+    with open(fpath,"r",encoding="utf-8") as f:
+        # All_Size=os.path.getsize(fpath)
+        All_Size=len(f.read())
+        if All_Size%Block_Size!=0:
+            total=All_Size//Block_Size+1
+        else:
+            total=All_Size//Block_Size
+        f.close()
+        return total
+
+
+def read_file(fpath,Block_Size):
+    with open(fpath,"r",encoding="utf-8") as f:
+        while True:
+            block = f.read(Block_Size)
+            if block:
+                yield block
+            else:
+                return
+    f.close()
+
+fpath='../scripts/move.lua'
+Block_Size =30
+print(total_count(fpath,Block_Size))
+count=0
+for i in read_file(fpath,Block_Size):
+    print(i)
+    count=count+1
+
+print(count)
+
+
